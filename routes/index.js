02ee8797d403d0c35cf4e2d4a71ba1b5b351ce6f
R <92724648+THOTARAKESH@users.noreply.github.com>
2022-01-06 00:37:25 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -14,17 +14,17 @@ router.post('/', function(req, res, next) {
   
   request(url, function(err, response, body) {
     if(err) {
-      console.error('There was an error trying to lookup weather data for ' + city + '.');
+      console.error('There was an error trying to lookup weather data for ' + country + '.');
       res.render('index', {weather: null, error: 'Error, please try again!'});
     } else {
       let weather = JSON.parse(body);
 
       if(weather.main == undefined) {
-        console.error('No weather data was avalable for ' + city + '.');
+        console.error('No weather data was avalable for ' + country + '.');
         res.render('index', {weather: null, error: 'Error, please try again!'});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        console.log("Found weather data for " + city + '.');
+        console.log("Found weather data for " + country + '.');
         res.render('index', {weather: weatherText, error: null});
       }
     }
